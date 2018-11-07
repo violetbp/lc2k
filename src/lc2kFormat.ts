412fd059cbb5d9b403b43f0d3990de7f2271b739
Violet Boyse-Peacor <violetbp@eecs.umich.edu>
2018-11-07 14:33:20 -0500

    Fixed optional indentation disabled the "manual" newline at end feature ...in favor of definition in package.json enabled range formatting (but its bad)

--- src/lc2kFormat.ts
+++ src/lc2kFormat.ts
@@ -4,6 +4,7 @@ import {TextDecoder} from 'util';
 export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider, vscode.DocumentFormattingEditProvider {
   provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions,
                                  token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+    // call it on the entire range
     let fullRange = new vscode.Range(0, 0, document.lineCount, 0);
     return this.provideDocumentRangeFormattingEdits(document, fullRange, options, token);
   }
@@ -28,7 +29,8 @@ export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider
     var regex;
     var newText;
     var offset;
-    for (let i = 0; i < document.lineCount; i++) {
+
+    for (let i = range.start.line; i < range.end.line; i++) {
       let line = document.lineAt(i);
       let text = line.text;
       var OpcodeRegex = /(?:[^\s]*\s+)(add|nor|nand|lw|sw|beq|jalr|noop|halt|\.fill)(?:.*)/g;
@@ -42,30 +44,30 @@ export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider
         case "lw":
         case "sw":
         case "beq":
-          regex = /^([^\s]*)\s+(add|nor|nand|lw|sw|beq)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+(.*)?/g;
-          newText = text.replace(regex, "$1\t$2\t$3\t$4\t$5\t$6");
+          regex = /^([^\s]*)\s+(add|nor|nand|lw|sw|beq)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
+          newText = text.replace(regex, "$1\t$2\t$3\t$4\t$5\t$7").trimRight();  // TODO add option to not format or change formatting of these
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
 
         case "jalr":
-          regex = /^([^\s]*)\s+(jalr)\s+([^\s]+)\s+([^\s]+)\s+(.*)?/g;
-          offset = ((jalrOffset !== 1) ? "\t".repeat(jalrOffset) : "");
-          newText = text.replace(regex, "$1\t$2\t$3\t$4\t" + offset + "$5");
+          regex = /^([^\s]*)\s+(jalr)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
+          offset = (jalrOffset ? "\t".repeat(jalrOffset + 1) : "$5");
+          newText = text.replace(regex, "$1\t$2\t$3\t$4" + offset + "$6").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
 
         case ".fill":
-          regex = /^([^\s]*)\s+(\.fill)\s+([^\s]+)\s+(.*)?/g;
-          offset = ((fillOffset !== 1) ? "\t".repeat(fillOffset) : "");
-          newText = text.replace(regex, "$1\t$2\t$3\t" + offset + "$4");
+          regex = /^([^\s]*)\s+(\.fill)\s+([^\s]+)(\s+)?(.*)?/g;
+          offset = (fillOffset ? "\t".repeat(fillOffset + 1) : "$4");
+          newText = text.replace(regex, "$1\t$2\t$3" + offset + "$5").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
 
         case "halt":
         case "noop":
-          regex = /^([^\s]*)\s+(halt|noop)\s+(.*)?/g;
-          offset = ((noopOffset !== 1) ? "\t".repeat(noopOffset) : "");
-          newText = text.replace(regex, "$1\t$2\t" + offset + "$3");
+          regex = /^([^\s]*)\s+(halt|noop)(\s+)?(.*)?/g;
+          offset = (noopOffset ? "\t".repeat(noopOffset + 1) : "$3");
+          newText = text.replace(regex, "$1\t$2" + offset + "$4").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
 
@@ -74,10 +76,10 @@ export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider
     }
 
     // last line not empty or whitespace and feature enabled //
-    const lastLine = document.lineAt(document.lineCount - 1);
-    if (!lastLine.isEmptyOrWhitespace && addNewLine) {
-      return [vscode.TextEdit.insert(lastLine.range.end, '\n')];
-    }
+    // const lastLine = document.lineAt(document.lineCount - 1);
+    // if (!lastLine.isEmptyOrWhitespace && addNewLine) {
+    //   return [vscode.TextEdit.insert(lastLine.range.end, '\n')];
+    // }
     return edits;
   }
 }
