5f52b1a53d2b019f13e877fea894b7e577182b9d
Violet Boyse-Peacor <violetbp@eecs.umich.edu>
2018-11-12 15:40:59 -0500

    using let instead of var now moved varables to inside loop

--- src/lc2kFormat.ts
+++ src/lc2kFormat.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import {TextDecoder} from 'util';
 
 export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider, vscode.DocumentFormattingEditProvider {
   provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions,
@@ -25,51 +24,54 @@ export class Lc2kFormatter implements vscode.DocumentRangeFormattingEditProvider
     // console.log("jalr offset: " + jalrOffset);
     // console.log("jalr offset: " + fillOffset);
 
-    var edits = [];
-    var regex;
-    var newText;
-    var offset;
+    let edits = [];
+
 
     for (let i = range.start.line; i < range.end.line; i++) {
       let line = document.lineAt(i);
       let text = line.text;
-      var OpcodeRegex = /(?:[^\s]*\s+)(add|nor|nand|lw|sw|beq|jalr|noop|halt|\.fill)(?:.*)/g;
+      let OpcodeRegex = /(?:[^\s]*\s+)(add|nor|nand|lw|sw|beq|jalr|noop|halt|\.fill)(?:.*)/g;
 
       let opcode = text.replace(OpcodeRegex, "$1");
       // console.log("opcode: " + opcode);
       // TODO add option to indent automatically on detecting opcode (with number after) at beginning of line
+      // TODO add option for spaces to seperate registers
       switch (opcode) {
         case "add":
         case "nor":
         case "lw":
         case "sw":
-        case "beq":
-          regex = /^([^\s]*)\s+(add|nor|nand|lw|sw|beq)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
-          newText = text.replace(regex, "$1\t$2\t$3\t$4\t$5\t$7").trimRight();  // TODO add option to not format or change formatting of these
+        case "beq": {
+          let regex = /^([^\s]*)\s+(add|nor|nand|lw|sw|beq)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
+          let newText = text.replace(regex, "$1\t$2\t$3\t$4\t$5\t$7").trimRight();  // TODO add option to not format or change formatting of these
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
+        }
 
-        case "jalr":
-          regex = /^([^\s]*)\s+(jalr)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
-          offset = (jalrOffset ? "\t".repeat(jalrOffset + 1) : "$5");
-          newText = text.replace(regex, "$1\t$2\t$3\t$4" + offset + "$6").trimRight();
+        case "jalr": {
+          let regex = /^([^\s]*)\s+(jalr)\s+([^\s]+)\s+([^\s]+)(\s+)?(.*)?/g;
+          let offset = (jalrOffset ? "\t".repeat(jalrOffset + 1) : "$5");
+          let newText = text.replace(regex, "$1\t$2\t$3\t$4" + offset + "$6").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
+        }
 
-        case ".fill":
-          regex = /^([^\s]*)\s+(\.fill)\s+([^\s]+)(\s+)?(.*)?/g;
-          offset = (fillOffset ? "\t".repeat(fillOffset + 1) : "$4");
-          newText = text.replace(regex, "$1\t$2\t$3" + offset + "$5").trimRight();
+        case ".fill": {
+          let regex = /^([^\s]*)\s+(\.fill)\s+([^\s]+)(\s+)?(.*)?/g;
+          let offset = (fillOffset ? "\t".repeat(fillOffset + 1) : "$4");
+          let newText = text.replace(regex, "$1\t$2\t$3" + offset + "$5").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
+        }
 
         case "halt":
-        case "noop":
-          regex = /^([^\s]*)\s+(halt|noop)(\s+)?(.*)?/g;
-          offset = (noopOffset ? "\t".repeat(noopOffset + 1) : "$3");
-          newText = text.replace(regex, "$1\t$2" + offset + "$4").trimRight();
+        case "noop": {
+          let regex = /^([^\s]*)\s+(halt|noop)(\s+)?(.*)?/g;
+          let offset = (noopOffset ? "\t".repeat(noopOffset + 1) : "$3");
+          let newText = text.replace(regex, "$1\t$2" + offset + "$4").trimRight();
           edits.push(vscode.TextEdit.replace(line.range, newText));
           break;
+        }
 
         default: { break; }
       }
